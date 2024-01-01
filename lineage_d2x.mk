f7866b6602b34f11a6bc7d14d8aee9089c5b11ff
Sideloader76 <155350390+Sideloader76@users.noreply.github.com>
2024-01-01 12:37:33 +0000

    lineage_d2x: adapt to AlphaDroid

--- lineage_d2x.mk
+++ lineage_d2x.mk
@@ -25,6 +25,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 ## Inscreen Fingerprint HAL
 TARGET_HAVE_FOD := true
 
+## Device config
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+## Gapps
+WITH_GMS := true
+TARGET_INCLUDE_GOOGLE_TELEPHONY := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_INCLUDE_GOOGLE_CAMERA := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_GMAIL := false
+TARGET_INCLUDE_GOOGLE_MAPS := true
+
+## Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Mgm2023
+
 ## S Pen
 TARGET_HAVE_SPEN := true
 
